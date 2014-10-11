8c3e9253cf7a8651c275a5358bc680c36345e402
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-10-11 16:28:21 +0200

    + some refactoring and fixes for data migration

--- TwitterSentimentAnalysis/migration.py
+++ TwitterSentimentAnalysis/migration.py
@@ -30,6 +30,10 @@ def get_config(name):
     return Config(f)
 
 
+def get_wait_time(cfg):
+    return 3600.0 / int(cfg.max_tweets_per_hour)
+
+
 def get_tweepy_api(cfg):
     tweepy.models.Status.first_parse = tweepy.models.Status.parse
     tweepy.models.Status.parse = parse
@@ -44,7 +48,6 @@ def get_tweepy_api(cfg):
 
 
 def read_total_list(in_filename):
-
     # read total fetch list csv
     fp = open(in_filename, 'rb')
     reader = csv.reader(fp, delimiter=',', quotechar='"')
@@ -56,8 +59,8 @@ def read_total_list(in_filename):
     return total_list
 
 
-def get_time_left_str(cur_idx, fetch_list, download_pause):
-    tweets_left = len(fetch_list) - cur_idx
+def get_time_left_str(cur_idx, length, download_pause):
+    tweets_left = length - cur_idx
     total_seconds = tweets_left * download_pause
     str_hr = int(total_seconds / 3600)
     str_min = int((total_seconds - str_hr*3600) / 60)
@@ -65,52 +68,49 @@ def get_time_left_str(cur_idx, fetch_list, download_pause):
     return '%dh %dm %ds' % (str_hr, str_min, str_sec)
 
 
-def download_tweets(fetch_list, raw_dir, tweeter_api, db):
-    # ensure raw data directory exists
-    if not os.path.exists(raw_dir):
-        os.mkdir(raw_dir)
+def save_tweet(db, item, status):
+    record = [{
+        '_id': item[2],
+        'topic': item[0],
+        'manual_grade': item[1],
+        'text': status.text,
+        'retweet_count': status.retweet_count,
+        'data': json.loads(status.json)
+        }]
+    db.test_tweets.insert(record)
 
-    # stay within rate limits
-    max_tweets_per_hr = 700
-    download_pause_sec = 3600 / max_tweets_per_hr
 
-    # download tweets
-    for idx in range(0, len(fetch_list)):
-        # current item
+def wait_between_requests(idx, length, download_pause_sec):
+    # stay in Twitter API rate limits
+    trem = get_time_left_str(idx, length, download_pause_sec)
+    print '    pausing %f sec to obey Twitter API rate limits (%s left)' % \
+          (download_pause_sec, trem)
+    time.sleep(download_pause_sec)
+
+
+def download_tweets(fetch_list, tweeter_api, db, download_pause_sec):
+    length = len(fetch_list)
+    for idx in range(0, length):
         item = fetch_list[idx]
-
-        # print status
-        trem = get_time_left_str(idx, fetch_list, download_pause_sec)
-        print '--> downloading tweet #%s (%d of %d) (%s left)' % \
-              (item[2], idx+1, len(fetch_list), trem)
 
         try:
             status = tweeter_api.get_status(id=item[2])
         except tweepy.error.TweepError, e:
-            if 'Rate limit exceeded' not in e.message:
+            if 'Rate limit exceeded!!' not in e.message:
                 print item[2], e.message
         else:
-            if db.test_tweets.find({'_id': item[2]}).count() == 0:          
-                # download to db
-                record = [{
-                    '_id': item[2],
-                    'topic': item[0],
-                    'manual_grade': item[1],
-                    'text': status.text,
-                    'retweet_count': status.retweet_count
-                    }]
-                db.test_tweets.insert(record)
-
-                # stay in Twitter API rate limits
-                print '    pausing %d sec to obey Twitter API rate limits' % \
-                      (download_pause_sec)
-                time.sleep(download_pause_sec)
+            if db.test_tweets.find({'_id': item[2]}).count() == 0:
+                print '--> downloading tweet #%s (%d of %d)' % \
+                      (item[2], idx+1, length)
+                save_tweet(db, item, status)
+                wait_between_requests(idx, length, download_pause_sec)
             else:
                 print 'Tweet ', item[2], ' already downloaded!\n'
     return
 
 
 def main():
+    print "Downloading tweets...\n"
 
     # get configuration
     cfg = get_config('configuration.cfg')
@@ -125,17 +125,14 @@ def main():
     api = get_tweepy_api(cfg)
 
     # fetch list and download tweets
-    print "Getting tweet list...\n"
     tweet_list = read_total_list(cfg.corpus_file)
-    print "Downloading tweets...\n"
-    download_tweets(tweet_list, cfg.raw_dir, api, db)
+    download_tweets(tweet_list, api, db, get_wait_time(cfg))
 
     # disconnect database
     client.close()
 
     # second pass for any failed downloads
     print '\nDownloading tweets done!\n'
-
     return
 
 
