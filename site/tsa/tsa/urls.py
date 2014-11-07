f51a1f8c32f1f889ddbd2f6a576c4d48713d4878
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-11-07 15:39:33 +0100

    add tweets searching and some layout things

--- site/tsa/tsa/urls.py
+++ site/tsa/tsa/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 # noinspection PyUnresolvedReferences
-from tweets.views import TweetSearchView
+from tweets.views import *
 
+
+# noinspection PyUnresolvedReferences
 urlpatterns = patterns('',
                        url(r'^$', TweetSearchView.as_view()),
-                       # url(r'^blog/', include('blog.urls')),
-
-                       url(r'^admin/', include(admin.site.urls)),)
+                       url(r'^contact/$', contact, name='contact'),
+                       url(r'^admin/', include(admin.site.urls)), )
--- site/tsa/tweets/views.py
+++ site/tsa/tweets/views.py
@@ -3,17 +3,37 @@ from django.views.generic import View
 import inject
 from TwitterSentimentAnalysis import core
 from config import Config
+from forms import QueryForm
 
 
-# noinspection PyMethodMayBeStatic
 class TweetSearchView(View):
+    template_name = "search.html"
+    language = 'en'
+
     @inject.params(config=Config)
     def __init__(self, config):
         self.cfg = config
         self.api = core.get_tweepy_api(self.cfg, False)
 
     def get(self, request):
-        records = self.api.home_timeline()[:10]
-        context = {'tweets': records}
-        template = "home.html"
-        return render(request, template, context)
+        records = self.api.home_timeline()
+        header = 'Home timeline'
+        context = {'tweets': records, 'form': QueryForm(), 'header': header}
+
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = QueryForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            records = self.api.search(query, self.language)
+            header = "Search: " + query
+            context = {'tweets': records, 'form': form, 'header': header}
+            return render(request, self.template_name, context)
+
+        header = 'Nothing found'
+        return render(request, self.template_name, {'form': form, 'header': header})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})
