03633cfdabea6b79a795b1ef3342b6e0d7c9f87f
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-11-11 21:23:32 +0100

    little pagination fixes

--- site/tsa/static/js/paginator.js
+++ site/tsa/static/js/paginator.js
@@ -3,3 +3,30 @@ function toPage(page)
     $('#id_page').val(page)
     $('#query_form').submit()
 }
+
+function toPrevious()
+{
+    if($('#id_page') > 1)
+    {
+        var page = $('#id_page').val() - 1
+        $('#id_page').val(page)
+        $('#query_form').submit()
+    }
+}
+
+function toNext()
+{
+    var page = $('#id_page').val() + 1
+    $('#id_page').val(page)
+    $('#query_form').submit()
+}
+
+function hideBackArrowIfInPageOne()
+{
+    if($('#id_page').val() == 1)
+    {
+        $('#pagination_back').hide()
+    }
+}
+
+$( document ).ready( hideBackArrowIfInPageOne );
