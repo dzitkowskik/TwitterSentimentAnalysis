63b98da5c61032a1aaac3797ab0f6c8d9990869a
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-11-12 20:58:52 +0100

    fixing unit tests

--- TwitterSentimentAnalysis/test/downloaders_test.py
+++ TwitterSentimentAnalysis/test/downloaders_test.py
@@ -4,24 +4,33 @@ from TwitterSentimentAnalysis import downloaders, core
 
 
 class DownloadTweetsTestCase(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         core.initialize('TwitterSentimentAnalysis/test/test_configuration.cfg')
+
+    @classmethod
+    def tearDownClass(cls):
+        core.terminate()
+
+    def setUp(self):
         self.tweet_downloader = downloaders.TweetDownloader()
         self.test_db = self.tweet_downloader.db
         self.test_table_name = "tweet_download_" + uuid.uuid4().hex + "_test"
 
     def tearDown(self):
-        core.terminate()
         self.test_db.drop_collection(self.test_table_name)
 
-    def test_download_tweets_using_query(self):
+    def test_download_tweets_using_query_check_tweet_number(self):
         expected = 50
         self.tweet_downloader.download_tweets_using_query("erasmus", expected, self.test_table_name, tag="test")
         number_of_tweets_in_db = self.test_db[self.test_table_name].count()
         self.assertEqual(number_of_tweets_in_db, expected)
-        tweet = self.test_db[self.test_table_name].find_one()
-        self.assertRegexpMatches(tweet["text"].lower(), "erasmus")
 
+    def test_download_tweets_using_query_check_text_not_empty(self):
+        self.tweet_downloader.download_tweets_using_query("erasmus", 1, self.test_table_name, tag="test")
+        tweet = self.test_db[self.test_table_name].find_one()
+        self.assertIsNotNone(tweet['text'])
+        self.assertNotEqual(tweet['text'], "")
 
 if __name__ == '__main__':
     unittest.main()
--- TwitterSentimentAnalysis/test/wordSentiment_test.py
+++ TwitterSentimentAnalysis/test/wordSentiment_test.py
@@ -4,18 +4,27 @@ from TwitterSentimentAnalysis import wordSentiment, core
 
 
 class WordSentimentAnalyzerTestCase(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         core.initialize('TwitterSentimentAnalysis/test/test_configuration.cfg')
+
+    @classmethod
+    def tearDownClass(cls):
+        core.terminate()
+
+    def setUp(self):
         words_dict = {'great': 3, 'noob': -2, 'super': 1, 'bad': -1}
         self.sentiment_analyzer = wordSentiment.WordSentimentAnalyzer()
         self.sentiment_analyzer.words_dict = words_dict
 
-    def tearDown(self):
-        core.terminate()
-
     def test_get_word_sentiment(self):
         result = self.sentiment_analyzer.get_word_sentiment('great job')
         expected = 3 / math.sqrt(2)
+        self.assertEqual(result, expected)
+
+    def test_get_word_sentiment_zero(self):
+        result = self.sentiment_analyzer.get_word_sentiment('fantasyland was owsome #creazy #idiot')
+        expected = 0
         self.assertEqual(result, expected)
 
 
