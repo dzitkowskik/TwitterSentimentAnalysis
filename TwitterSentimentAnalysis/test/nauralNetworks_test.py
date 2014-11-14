dc3eccfcb90a59c2be43cf30b97286c7a1e66e7a
Matthias Baetens <baetensmatthias@gmail.com>
2014-11-14 22:53:38 +0100

    neural networks test

--- TwitterSentimentAnalysis/test/nauralNetworks_test.py
+++ TwitterSentimentAnalysis/test/nauralNetworks_test.py
@@ -1,6 +1,8 @@
 import uuid
 import unittest
-from TwitterSentimentAnalysis import neuralNetworks, core
+from TwitterSentimentAnalysis import neuralNetworks, core, downloaders, datasets
+from pybrain.utilities import percentError
+from pybrain.supervised.trainers import BackpropTrainer
 from config import Config
 import os
 
@@ -14,3 +16,42 @@ class NeuralNetworksTweetsTestCase(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         core.terminate()
+
+    def setUp(self):
+        self.tweet_downloader = downloaders.TweetDownloader()
+        self.tweetclassificationdataset = datasets.TweetClassificationDatasetFactory()
+        self.test_db = self.tweet_downloader.db
+        self.test_table_name = "tweet_download_" + uuid.uuid4().hex + "_test"
+
+    def setUp(self):
+        self.tweet_downloader = downloaders.TweetDownloader()
+        self.test_db = self.tweet_downloader.db
+        self.test_table_name = "tweet_download_" + uuid.uuid4().hex + "_test"
+
+    def test_MultiClassClassificationNeuralNetwork(self):
+        neural_network = neuralNetworks.MultiClassClassificationNeuralNetwork(3, 1)
+        self.tweet_downloader.download_tweets_using_query("erasmus", 100, self.test_table_name, tag = "erasmus")
+        ds = self.tweetclassificationdataset.get_dataset(self.test_table_name)
+
+        self.assertIsNotNone(neural_network.network)
+
+        ds_train, ds_test = ds.splitWithProportion(0.75)
+
+        result = neural_network.run(ds_train, ds_test)
+
+        trainer = BackpropTrainer(
+            self.network,
+            dataset=ds_train,
+            momentum=0.1,
+            verbose=True,
+            weightdecay=0.01)
+
+        trainer.trainEpochs(self.epochs)
+        tstresult = percentError(
+            trainer.testOnClassData(dataset=ds_test),
+            ds_test['class'])
+
+        self.assertEqual(result, tstresult)
+
+
+
