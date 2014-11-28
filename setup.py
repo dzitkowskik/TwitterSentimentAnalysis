2e1535d7cc145c000949aa31d5d14f103c997c92
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-11-28 12:57:57 +0100

    -+ Move migration.py content to setup.py - remove tweetSentiment.py

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
 
-from TwitterSentimentAnalysis import migration
-migration.main()
+# Karol Dzitkowski
+# k.dzitkowski@gmail.com
+# 10-10-2014
+__author__ = 'Karol Dzitkowski'
+
+from config import Config
+from TwitterSentimentAnalysis import core, downloaders
+
+core.initialize()
+print "Downloading tweets...\n"
+
+cfg = Config(core.configuration_file_path)
+downloaders.TweetDownloader().download_tweets_from_file(table_name=cfg.train_tweets_table)
+
+print '\nDownloading tweets done!\n'
+core.terminate()
