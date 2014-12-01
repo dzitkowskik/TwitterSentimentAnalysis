49d836030d8bd8650b4e092bf85f6262c4ec52fa
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-12-01 16:44:22 +0100

    documentation of apps.py

--- site/tsa/tweets/apps.py
+++ site/tsa/tweets/apps.py
@@ -3,8 +3,13 @@ from TwitterSentimentAnalysis import core
 
 
 class TweetsAppConfig(AppConfig):
+    """
+    This class is used to perform an TweetSentimentAnalysis
+    initialization before using it in tweets app in web site
+    """
     name = 'tweets'
     verbose_name = "TweetsAppConfig"
 
     def ready(self):
+        """Initializes TweetSentimentAnalysis package"""
         core.initialize()
